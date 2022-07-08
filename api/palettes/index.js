519b12447a89861c297e9246fb839a7f2e7b4479
Magdalena Kierat <m.kierat@gmx.de>
2022-07-08 15:29:41 +0200

    palettes request handler

---
+++ api/palettes/index.js
@@ -0,0 +1,34 @@
+import connectToMongodb from "../_database/connect-to-mongodb";
+import { Palette } from "../_database/models/index.js";
+import mongoose from "mongoose";
+
+const handler = async (request, response) => {
+  try {
+    await connectToMongodb();
+
+    if (request.method === "GET") {
+      const palette = await Palette.findOne({});
+      return response.json(palette);
+    } else if (request.method === "POST") {
+      const newPalette = await Palette.findOneAndReplace(
+        { user: mongoose.Types.ObjectId(request.body.user) },
+        {
+          user: mongoose.Types.ObjectId(request.body.user),
+          name: request.body.palette.name,
+          savedAt: Date.now(),
+          colors: request.body.palette.colors,
+        },
+        {
+          upsert: true,
+          returnDocument: "after",
+        }
+      );
+      return response.json(newPalette);
+    } else {
+      throw new Error("Route not supported");
+    }
+  } catch (error) {
+    return response.status(500).json({ message: "Server Error" });
+  }
+};
+export default handler;
